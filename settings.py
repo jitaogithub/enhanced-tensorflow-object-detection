640f336fd3dca1b1a65f3df09df4abe95ffa5991
Tao Ji <jitaolive@outlook.com>
2018-02-19 21:49:06 -0600

    Minor bug fix

--- settings.py
+++ settings.py
@@ -11,10 +11,10 @@ PATH_TO_CKPT = os.path.join(MODEL_DIR, MODEL_NAME)
 
 # Label information
 PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
-NUM_CLASSES = 90
+NUM_CLASSES = 14
 
 # Post-process configuration
-THRESHOLD = 0.22
+THRESHOLD = 0.3
 BOX_COLOR = (0, 255, 0) # Support for lambda expression on score and class id to be added
 BOX_THICKNESS = 2
 FONT_FACE = 0  # cv2.FONT_HERSHEY_SIMPLEX
@@ -24,16 +24,20 @@ FONT_THICKNESS = 1
 
 # I/O configuration
 INPUT_DIR = 'input'
+
 VIDEO_SET = 'sample_video'
 VIDEO_NAME = 'sample.mp4'
 VIDEO_PATH = os.path.join(INPUT_DIR, VIDEO_SET, VIDEO_NAME)
+
 IMAGE_SET = 'sample_images'
-IMAGE_PATHS = [ os.path.join(INPUT_DIR, IMAGE_SET, file)  \
-    for file in os.listdir(os.path.join(INPUT_DIR, IMAGE_SET)) \
-    if re.match('.*\.(jpg|jpeg|png)$', file, re.IGNORECASE)]
-IMAGE_NUM = len(IMAGE_PATHS)
-OUTPUT_DIR = os.path.join('output', IMAGE_SET)
-if not os.path.exists('output'):
-    os.mkdir('output')
-if not os.path.exists(OUTPUT_DIR):
-    os.mkdir(OUTPUT_DIR)
+
+if os.path.exists(os.path.join(INPUT_DIR, IMAGE_SET)):
+    IMAGE_PATHS = [ os.path.join(INPUT_DIR, IMAGE_SET, file)  \
+        for file in os.listdir(os.path.join(INPUT_DIR, IMAGE_SET)) \
+        if re.match('.*\.(jpg|jpeg|png)$', file, re.IGNORECASE)]
+    IMAGE_NUM = len(IMAGE_PATHS)
+    OUTPUT_DIR = os.path.join('output', IMAGE_SET)
+    if not os.path.exists('output'):
+        os.mkdir('output')
+    if not os.path.exists(OUTPUT_DIR):
+        os.mkdir(OUTPUT_DIR)
